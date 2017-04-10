8902c95657038ccd75a6c178d14a59107b66fde8
Thomas Brand <tom@trellis.ch>
2017-04-11 01:06:56 +0200

    allow to run script from any directory

--- libs/surfaces/cso/create_cso_script_h.sh
+++ libs/surfaces/cso/create_cso_script_h.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+#//tb/1704
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #create_cso_script_h.sh > cso_script.h
 
-md5="`md5sum cso.lua`"
+md5="`md5sum ${DIR}/cso.lua`"
 cat - << __EOF__
 #ifndef ardour_cso_script_h
 #define ardour_cso_script_h
@@ -12,7 +16,7 @@ cat - << __EOF__
 const std::string CSO_SCRIPT = 
 __EOF__
 echo -n '"'
-cat cso.lua | base64 -w0
+cat "$DIR"/cso.lua | base64 -w0
 echo '";'
 cat - << __EOF__
 #endif
